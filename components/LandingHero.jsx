60a6a55929d89611e988993e5094e85fbac58aec
jain12feb <jain00prince@gmail.com>
2023-08-02 03:42:54 +0530

    commit

--- components/LandingHero.jsx
+++ components/LandingHero.jsx
@@ -8,7 +8,7 @@ const LandingHero = () => {
       <div className="grid max-w-screen-xl px-4 py-8 mx-auto lg:gap-8 xl:gap-0 lg:py-16 lg:grid-cols-12">
         <div className="lg:mr-auto lg:place-self-center lg:col-span-7">
           <span className="max-w-2xl mb-4 text-4xl font-extrabold tracking-tight leading-none md:text-5xl xl:text-6xl ">
-            Everything you need to build a Great{" "}
+            Everything <br /> you need to build a <br /> Great{" "}
             <TypewriterComponent
               component={"span"}
               options={{
